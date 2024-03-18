794e3b4e2c525a655fc8b18ec24c71ea808a4ee9
mairarafiq301 <mairarafiq301@gmail.com>
2024-03-18 01:52:56 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let weight_in_kg = 55;
+let height_in_meters = 5.3;
+let BMI = weight_in_kg / height_in_meters ** 2;
+console.log(`MY BMI:  ${BMI}.`);
---
+++ app.ts
@@ -0,0 +1,4 @@
+let weight_in_kg :number = 55 ;
+let height_in_meters:number = 5.3 ;
+let BMI =  weight_in_kg / height_in_meters ** 2 ;
+console.log(`MY BMI:  ${BMI}.`) ;
